06eda2230dea2d8456e2aa446126d454e8a5cac7
Max94 <maxy_14@hotmail.it>
2023-02-16 18:44:21 +0000

    First commitment with class created

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+class Battleship:
+    """
+    Game class. The class will provide the player with the choice
+    to create the grid with as many rows and columns he wants from
+    a minimum of 4 to a maximum of 8, for playability reasons.
+    """
+    def __init__(self, size, ships, turns):
+        self.size = size
+        self.ships = ships
+        self.turns = turns
+
