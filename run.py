bebca6a591bb38cd179219f5c1d3fd12ba66c65d
Max94 <maxy_14@hotmail.it>
2023-02-16 19:18:31 +0000

    Added for loop and method to create ships, testing

--- run.py
+++ run.py
@@ -18,5 +18,18 @@ class Battleship:
         for i in range(size):
             self.board.append(["O"] * size)
 
+        for i in range(ships):
+            row, col = self._generate_ship_location()
+            self.ship_row.append(row)
+            self.ship_col.append(col)
+
+    def _generate_ship_location(self):
+        while True:
+            row = randint(0, self.size - 1)
+            col = randint(0, self.size - 1)
+            if row not in self.ship_row and col not in self.ship_col:
+                return row, col
+            
+
 
 Battleship(5, 3, 5)
